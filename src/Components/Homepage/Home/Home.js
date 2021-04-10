60392837edfbf47e2fe12ff7f0b3e1c550455528
PC-coding <partapchauhan8@gmail.com>
2021-04-10 00:12:56 -0400

    working on homepage, installed react-router-dom

---
+++ src/Components/Homepage/Home/Home.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Home.css';
+import { Link } from 'react-router-dom';
+
+function Home() {
+    return (
+        <div>
+            <h1> This is the homepage </h1>
+
+            <div className='home_header'>
+                <div className='home_headerLeft'>
+                    <Link to='/about'>About</Link>
+                    <Link to='/store'>Store</Link>
+                </div>
+                <div className='home_headerRight'>
+                    <Link to='/gmail'>Gmail</Link>
+                    <Link to='/images'>Images</Link>
+                </div>
+            </div>
+
+            <div className='home_body'>
+
+            </div>
+        </div>
+    )
+}
+
+export default Home
