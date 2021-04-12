969b98ecd8b25a23ae15df63c7528090c883b162
PC-coding <partapchauhan8@gmail.com>
2021-04-11 22:50:50 -0400

    css updates

--- src/Components/Homepage/Home/Home.js
+++ src/Components/Homepage/Home/Home.js
@@ -21,10 +21,7 @@ function Home() {
             </div>
 
             <div className='home_body'>
-                {/* <img
-                    src='https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png'
-                    alt='' 
-                /> */}
+                
             </div>
         </div>
     )
