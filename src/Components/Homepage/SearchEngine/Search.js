86976bfcaed4b34b817a31d23be2d7245996ca22
PC-coding <partapchauhan8@gmail.com>
2021-04-13 20:49:56 -0400

    working on search func

--- src/Components/Homepage/SearchEngine/Search.js
+++ src/Components/Homepage/SearchEngine/Search.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Search.css';
 import SearchIcon from '@material-ui/icons/Search';
 import MicIcon from '@material-ui/icons/Mic'
 import { Button } from '@material-ui/core'
 
 function Search() {
+    const [input, setInput] = useState('');
+
+    const search = e => {
+        e.preventDefault();
+    }
+    
     return (
         <div className='search'>
             <div className='search_input'>
@@ -13,7 +19,7 @@ function Search() {
                 <MicIcon />
             </div>
             <div className='search_buttons'>
-                <Button variant='outlined'>Looker Search</Button>
+                <Button onClick={search} variant='outlined'>Looker Search</Button>
                 <Button variant='outlined'>I'm Feeling Fortunate</Button>
             </div>
         </div>
