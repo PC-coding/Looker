dab878723050f2ab24c0a6c456cfb73dc82a361b
PC-coding <partapchauhan8@gmail.com>
2021-04-16 13:32:04 -0400

    updates on search engine

--- src/Components/Homepage/SearchEngine/useGoogleSearch.js
+++ src/Components/Homepage/SearchEngine/useGoogleSearch.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from 'react';
+import API_KEY from '../keys';
 
-const useGoogleSearch() => {
+const CONTEXT_KEY = '453e1b921e389719c';
 
+const useGoogleSearch = () => {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async() => {
+            fetch(
+                `https://googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${term}`
+            )
+        }
+    }, [term])
+
+    return { data}
 }
 
 export default useGoogleSearch
---
+++ src/Components/Homepage/keys.js
@@ -0,0 +1,3 @@
+export const API_KEY = 'AIzaSyCyM7VAM2HKdPsgPJhBHQZQl14jA-Kt66c';
+
+export default API_KEY;
