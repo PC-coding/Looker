9bce667544b4983ac89f05cb34ced8a9c07c5d36
PC-coding <partapchauhan8@gmail.com>
2021-04-15 20:51:41 -0400

    imported stateprovider and reducer to index

--- src/reducer.js
+++ src/reducer.js
@@ -8,12 +8,14 @@ export const actionTypes = {
 
 const reducer = (state, action) => {
     console.log(action);
+
     switch(action.type) {
         case actionTypes.SET_SEARCH_TERM:
             return {
                 ...state,
                 term: action.term,
             };
+            
         default:
             return state;
     }
