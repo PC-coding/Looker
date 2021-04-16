72353fc53b6e6a206167795c9eb6cdd44884dda1
PC-coding <partapchauhan8@gmail.com>
2021-04-16 13:34:44 -0400

    debugging search engine

--- src/Components/Homepage/SearchEngine/SearchPage.js
+++ src/Components/Homepage/SearchEngine/SearchPage.js
@@ -7,6 +7,7 @@ function SearchPage() {
     const [{ term }, dispatch] = useStateValue();
     const { data } = useGoogleSearch(term);
 
+    console.log(data)
     return (
         <div className='searchPage'>
             <div className='searchPage_header'>
