5263ea13cc35ebe6134d078312fafa90396a754f
PC-coding <partapchauhan8@gmail.com>
2021-04-16 13:33:56 -0400

    imported google search;

--- src/Components/Homepage/SearchEngine/useGoogleSearch.js
+++ src/Components/Homepage/SearchEngine/useGoogleSearch.js
@@ -12,6 +12,9 @@ const useGoogleSearch = () => {
                 `https://googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${term}`
             )
             .then(response => response.json())
+            .then(result => {
+                setData(result)
+            })
         }
     }, [term])
 
