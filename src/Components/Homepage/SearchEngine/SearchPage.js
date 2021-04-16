c2a8e16a3ad06b43df62ce52e9b4a0a9665df3d4
PC-coding <partapchauhan8@gmail.com>
2021-04-16 13:20:02 -0400

    providing state in searchpage

--- src/Components/Homepage/SearchEngine/SearchPage.js
+++ src/Components/Homepage/SearchEngine/SearchPage.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import './SearchPage.css';
+import { useStateValue } from '../../../StateProvider';
 
 function SearchPage() {
+    const [{ term }, dispatch] = useStateValue();
+
     return (
-        <div>
-            
+        <div className='searchPage'>
+            <div className='searchPage_header'>
+
+            </div>
+            <div className='searchPage_results'>
+
+            </div>
         </div>
     )
 }
