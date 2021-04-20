2d5c18f214eb88f0d927a209b473d3df5c1cfd51
PC-coding <partapchauhan8@gmail.com>
2021-04-20 10:14:16 -0400

    working on search page option icons

--- src/Components/Homepage/SearchEngine/SearchPage.js
+++ src/Components/Homepage/SearchEngine/SearchPage.js
@@ -5,6 +5,7 @@ import useGoogleSearch from './useGoogleSearch';
 import { Link } from 'react-router-dom';
 import searchLogo from '../../../Looker_logo.png';
 import Search from './Search';
+import SearchIcon from '@material-ui/icons/Search';
 
 function SearchPage() {
     const [{ term }, dispatch] = useStateValue();
@@ -20,7 +21,15 @@ function SearchPage() {
             
                 <div className='searchPage_headerBody'>
                     <Search hideButtons />  
+                    <div className='searchPage_options'>
+                        <SearchIcon />
+                        <Link to='/all'>All</Link>
+                    </div>
                 </div>
+            </div>
+
+            <div className='searchPage_results'>
+
             </div>
         </div>
     )
