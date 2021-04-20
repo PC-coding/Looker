54867808b5287330fd92ca1298db170d0f0de75d
PC-coding <partapchauhan8@gmail.com>
2021-04-20 10:19:35 -0400

    added more links and imports

--- src/Components/Homepage/SearchEngine/SearchPage.js
+++ src/Components/Homepage/SearchEngine/SearchPage.js
@@ -6,6 +6,11 @@ import { Link } from 'react-router-dom';
 import searchLogo from '../../../Looker_logo.png';
 import Search from './Search';
 import SearchIcon from '@material-ui/icons/Search';
+import DescriptionIcon from '@material-ui/icons/Description';
+import ImageIcon from '@material-ui/icons/Image';
+import LocalOfferIcon from '@material-ui/icons/LocalOffer';
+import RoomIcon from '@material-ui/icons/Room';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 function SearchPage() {
     const [{ term }, dispatch] = useStateValue();
@@ -24,6 +29,26 @@ function SearchPage() {
                     <div className='searchPage_options'>
                         <SearchIcon />
                         <Link to='/all'>All</Link>
+                    </div>
+                    <div className='searchPage_options'>
+                        <DescriptionIcon />
+                        <Link to='/news'>News</Link>
+                    </div>
+                    <div className='searchPage_options'>
+                        <ImageIcon />
+                        <Link to='/images'>Images</Link>
+                    </div>
+                    <div className='searchPage_options'>
+                        <LocalOfferIcon />
+                        <Link to='/shopping'>Shopping</Link>
+                    </div>
+                    <div className='searchPage_options'>
+                        <RoomIcon />
+                        <Link to='/maps'>Maps</Link>
+                    </div>
+                    <div className='searchPage_options'>
+                        <MoreVertIcon />
+                        <Link to='/more'>More</Link>
                     </div>
                 </div>
             </div>
